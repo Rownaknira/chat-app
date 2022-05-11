12d42118d6a17b3f62bd17fcab73e2c567356504
Rownaknira <rownaknira@yahoo.com>
2022-05-11 22:52:28 +0600

    Implement basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Topbar } from './components/topbar';
+import { Navbar } from './components/navbar';
+import styles from './styles/App.module.scss';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Topbar />
+      <div className={styles.main}>
+        <div className={styles.main__left}><Navbar /></div>
+        <div className={styles.main__right}></div>
+      </div>
     </div>
   );
 }
